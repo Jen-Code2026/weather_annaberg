dbddd29f29a66d2fe4e4773d108edf89efdf6742
Ninu Suen <314787904+Jen-Code2026@users.noreply.github.com>
2026-08-19 22:56:29 +0200

    Loading the starting point into my new repository

---
+++ weather_annaberg.py
@@ -0,0 +1,229 @@
+"""
+Goal:
+Weather app for Annaberg, Lower Austria, with push notifications and a website
+
+Was muss das Programm können (auf Deutsch, weil das nur meine eigene Übersicht ist und nicht drin bleibt):
+
+1. Aktuelles Wetter anzeigen
+2. Vorhersage für heute, morgen und übermorgen treffen können
+3. Die Vorhersagen in frühe Morgenstunden, morgens - vormittags, mittags, nachmittags, abends - nachts unterteilen, weil sonst die Aussagen nutzlos sind für den Liftbetrieb
+4. Bei Gewitter und Sturm(böen) die Zeit dazu ausspucken (wegen Liftbetrieb)
+5. Liftbetrieb ist von 8 - 17 Uhr, im Winter von 8 - 16 Uhr. Das muss bei Vorhersagen dazu bedacht werden.
+
+Wenn das läuft:
+
+6. 5 verschiedene Modelle von Open Meteo vergleichen, Mittelwert ziehen 
+7. auf 2 verschiedene Anbieter erweitern, je verschiedene Modelle berechnen, daraus dann absoluten Mittelwert ziehen
+
+---------------------
+
+To do:
+
+1. Funktionierende Funktion schreiben. Dann darauf aufbauen.
+
+enthalten sein müssen:
+Temperaturen (min, max, gefühlt)
+Gewitterwahrscheinlichkeit
+Regen/Schneewahrscheinlichkeit
+Wind (Geschwindigkeit, Richtung, Böen)
+
+Das Ergebnis soll sein:
+
+16.08.2026:
+
+Wetter: überwiegend sonnig
+Regen/Schnee: unwahrscheinlich, am wahrscheinlichsten noch zwischen 15 - 16 Uhr
+Gewitter: wahrscheinlich, zwischen 15 - 16 Uhr
+Wind: überwiegend schwach aus Nordosten, zwischen 15 - 16 Uhr kräfige Böen möglich
+Liftbetrieb: vorraussichtlich eingeschränkt
+
+Dh:
+
+1. ich muss 
+"""
+
+import requests #for the api datas
+import weather_functions as wf
+from datetime import date, timedelta #we need dates like "Today the 15.08.2026"
+
+#we need the different dates to make exact forecasts
+today_us = date.today()
+today = today_us.strftime("%d.%m.%Y")
+tomorrow_us = today_us + timedelta(days=1)
+tomorrow = tomorrow_us.strftime("%d.%m.%Y")
+da_tomorrow_us = today_us + timedelta(days=2)
+day_after_tomorrow = da_tomorrow_us.strftime("%d.%m.%Y")
+today_api = today_us.isoformat()
+
+url = "https://api.open-meteo.com/v1/forecast"
+#url2 = "https://dataset.api.hub.geosphere.at/v1/datasets" 
+
+parameter = {
+    "latitude": 47.8717277,
+    "longitude": 15.3760583,
+    "current": ",".join([  #data for today
+        "temperature_2m",
+        "apparent_temperature",
+        "precipitation_probability",
+        "weather_code",
+        "wind_speed_10m",
+        "wind_direction_10m",
+        "wind_gusts_10m",
+    ]),
+
+    "hourly": ",".join([ #hourly forecast
+        "temperature_2m",
+        "apparent_temperature",
+        "precipitation_probability",
+        "weather_code",
+        "wind_speed_10m",
+        "wind_direction_10m",
+        "wind_gusts_10m",
+    ]),
+
+    "daily": ",".join([ #forecast for tomorrow
+        "temperature_2m_min",
+        "temperature_2m_max",
+        "apparent_temperature_min",
+        "apparent_temperature_max",
+        "precipitation_probability_max",
+        "weather_code",
+        "wind_speed_10m_max",
+        "wind_direction_10m_dominant",
+        "wind_gusts_10m_max",
+    ]),
+
+    "wind_speed_unit": "kmh", 
+    "timezone": "Europe/Vienna",
+    "forecast_days": 3 #until incl. the day after tomorrow
+}
+
+result = requests.get(url, params=parameter) 
+
+if not result.ok: #debugging
+    print(result.json()) #what happens when we let us show "result"
+    result.raise_for_status() #successful would be 200
+
+data = result.json() #the json file with all the data
+
+
+#TEMPERATURE-PART--------------------------------------------------------------------------
+
+times = data["hourly"]["time"]
+temps = data["hourly"]["temperature_2m"]
+    
+wf_morning, wf_late_morning, wf_noon, wf_afternoon, wf_evening = wf.temperature_function(times, temps, today_api)
+
+morning = wf.average_values(wf_morning)
+late_morning = wf.average_values(wf_late_morning)
+noon = wf.average_values(wf_noon)
+afternoon = wf.average_values(wf_afternoon)
+evening = wf.average_values(wf_evening)
+
+print(f"Temperaturen heute: Morgens: {morning} Grad, vormittags: "
+      f"{late_morning} Grad, mittags: {noon} Grad, nachmittags: "
+      f"{afternoon} Grad, abends: {evening} Grad")
+
+
+
+#TEST-PART--------------------------------------------------------------------------
+
+"""
+
+
+times= data["hourly"]["time"]
+wind_speeds = data["hourly"]["wind_speed_10m"]
+tomorrow_api = tomorrow_us.isoformat() #das Datumsformat, mit dem die Datennamen 
+                                       #der einzelnen Stunden anfangen
+
+print(times)
+
+for time, wind_speed in zip(times, wind_speeds):
+    if time.startswith(tomorrow_api):
+        print(time, wind_speed)
+
+"""
+#TEST-PART-ENDE--------------------------------------------------------------------------
+
+#---- Function for wind direction ---------------------------------------------------
+#we want to have a word like southeast as a direction instead of just "225"
+
+"""def direction_function(direction):
+    if direction >= 337.5 or direction < 22.5:
+        return "Norden"
+    elif direction < 67.5:
+        return "Nordosten"
+    elif direction < 112.5:
+        return "Osten"
+    elif direction < 157.5:
+        return "Südosten"
+    elif direction < 202.5:
+        return "Süden"
+    elif direction < 247.5:
+        return "Südwesten"
+    elif direction < 292.5:
+        return "Westen"
+    elif direction < 337.5:
+        return "Nordwesten"
+
+current_direction = data["current"]["wind_direction_10m"]
+today_direction = data["daily"]["wind_direction_10m_dominant"][0]
+tomorrow_direction = data["daily"]["wind_direction_10m_dominant"][1]
+dat_direction = data["daily"]["wind_direction_10m_dominant"][2]
+
+current_winddirection = direction_function(current_direction)
+today_winddirection = direction_function(today_direction)
+tomorrow_winddirection = direction_function(tomorrow_direction)
+dat_winddirection = direction_function(dat_direction)
+
+#---- Function for wind speed -------------------------------------------------------
+
+def speed_function(speed):
+    if speed < 1:
+        return "kein Wind."
+    elif speed < 6:
+        return f"ein leiser Zug mit {speed} km/h aus"
+    elif speed < 12:
+        return f"eine leichte Brise mit {speed} km/h aus"
+    elif speed < 20:
+        return f"ein schwacher Wind mit {speed} km/h aus"
+    elif speed < 29:
+        return f"ein mäßiger Wind mit {speed} km/h aus"
+    elif speed < 39:
+        return f"ein frischer Wind mit {speed} km/h aus"
+    elif speed < 50:
+        return f"ein starker Wind mit {speed} km/h aus"
+    elif speed < 62:
+        return f"ein steifer Wind mit {speed} km/h aus"
+    elif speed < 75:
+        return f"ein stürmischer Wind mit {speed} km/h aus"
+    elif speed < 89:
+        return f"Sturm mit {speed} km/h aus"
+    elif speed < 103:
+        return f"schwerer Sturm mit {speed} km/h aus"
+    elif speed < 118:
+        return f"ein orkanartiger Sturm mit {speed} km/h aus"
+    else:
+        return f"ein Orkan mit {speed} km/h aus"
+
+current_speed = data["current"]["wind_speed_10m"]
+current_windspeed = speed_function(current_speed)
+
+
+#---- THE FORECAST -----------------------------------------------------------------
+print("Aktuelles Wetter:")
+print(f"Gerade weht {current_windspeed} {current_winddirection}.")
+print("")
+print(f"Wetter heute am {today}:")
+
+print("")
+print(f"Wetter morgen am {tomorrow}:")
+
+print("")
+print(f"Voraussichtliches Wetter übermorgen am {day_after_tomorrow}:")
+
+print("")
+
+
+
+"""
---
+++ weather_functions.py
@@ -0,0 +1,64 @@
+#functions to import into the main source file
+
+"""
+Idee:
+
+zeit:
+
+start des tages: 0 - 6 uhr
+vormittags: 6 - 11 uhr
+mittags: 11 - 14 uhr
+nachmittags: 14 - 19 uhr
+abends: 19 - 0 uhr
+
+wettercodes zb. so ausgeben:
+wenn 1 - 2 gleich und zb regen, und 3 - 5 auch gleich und kein regen: startet regnerisch, gegen mittag hört der regen auf
+wenn 1, 3 und 5 gleich: immer wieder kommt es zu regen 
+
+mögliche Kombinationen:
+
+1 - 5 gleich: ganzer Tag
+1 - 4 gleich: ganzen Tag, gegen Abend ...
+2 - 5 gleich: startet x, dann den Rest des Tages yp
+1 - 3 und 4 - 5 gleich: bis Mittag x, ab Nachmittag y
+1 - 2 gleich 3 - 5 gleich: startet x, ab Mittag y
+1 - 3 + 5 gleich: den ganzen Tag, nachmittags (vormittags, mittags) unterbrochen von (das geht für alle Kombis in der Art: 1 + 3 - 5 gleich, 1 + 2 + 4 + 5 gleich)
+1 - 3 gleich, 4 und 5 jeweils anders: bis zum frühen Nachmittag x, danach wechselhaft
+nichts gleich oder nur max. 2 Werte gleich: Wetter wechselhaft, höchste Niederschlagswahrscheinlichkeit gegen zb. Nachmittag
+3 x dasselbe Wetter, unzusammenhängend: im Tagesverlauf immer wieder x, unterbrochen von y (unterscheiden sich die beiden anderen punkte auch: + gegen Mittag und z am Abend)
+2 - 4 gleich, 1 und 5 unterschiedlich: startet x, dann über den Tag y, zum Abend hin z
+2 - 4 gleich, 1 + 5: startet mit x, über den Tag y, gegen Abend wieder x
+3 - 5 gleich, 1 und 2 verschieden: startet wechselhaft, ab Mittag dann
+
+
+"""
+
+def temperature_function(times, temps, target_date):
+    wf_morning = []
+    wf_late_morning = []
+    wf_noon = []
+    wf_afternoon = []
+    wf_evening = []
+
+    for time, temp in zip(times, temps):
+        if time.startswith(target_date):
+            hour_raw = time[11:13]
+            hour = int(hour_raw)
+
+            if hour in range(0, 6):
+                wf_morning.append(temp)
+            elif hour in range(6, 11):
+                wf_late_morning.append(temp)
+            elif hour in range(11, 14):
+                wf_noon.append(temp)
+            elif hour in range(14, 19):
+                wf_afternoon.append(temp)
+            else:
+                wf_evening.append(temp)
+
+    return wf_morning, wf_late_morning, wf_noon, wf_afternoon, wf_evening
+
+def average_values(werte):
+    average_raw = sum(werte) / len(werte)
+    average = round(average_raw, 1)
+    return average
